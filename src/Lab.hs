c660152bd18beec1510e58d5e5c43fa5de8e45e8
Matthew Hale <Matth3w4000@gmail.com>
2021-01-28 22:42:21 +0000

    solutions

--- src/Lab.hs
+++ src/Lab.hs
@@ -10,38 +10,32 @@ module Lab where
 data IntPos = IntPos Int Int 
 
 instance Eq IntPos where 
-    (IntPos x0 y0) == (IntPos x1 y1) = undefined 
+    (IntPos x0 y0) == (IntPos x1 y1) = (x0 == x1) && (y0 == y1)
 
 instance Show IntPos where 
-    show (IntPos x y) = undefined 
+    show (IntPos x y) = "IntPos" ++ show x ++ " " ++ show y
 
 zeroPos :: IntPos 
-zeroPos = undefined 
+zeroPos = IntPos 0 0
 
 x :: IntPos -> Int 
-x = undefined 
+x (IntPos a b) = a
 
 y :: IntPos -> Int 
-y = undefined 
+y (IntPos a b) = b
 
 --------------------------------------------------------------------------------
 
-data Pos a = Pos a a 
+data Pos a = Pos {left :: a, top :: a}
 
 instance Eq a => Eq (Pos a) where 
-    (Pos x0 y0) == (Pos x1 y1) = undefined 
+    (Pos x0 y0) == (Pos x1 y1) = (x0 == x1) && (y0 == y1)
 
 instance Show a => Show (Pos a) where 
-    show (Pos x y) = undefined
+    show (Pos x y) = "Pos" ++ show x ++ " " ++ show y
 
 zero :: Num a => Pos a 
-zero = undefined 
-
-left :: Pos a -> a 
-left = undefined 
-
-top :: Pos a -> a 
-top = undefined 
+zero = Pos 0 0
 
 --------------------------------------------------------------------------------
 
@@ -55,6 +49,7 @@ doc =
     ]
 
 render :: [DocumentItem] -> Int -> String 
-render = undefined
+render [] _ = ""
+render (ListItem func:funcs) n = func n ++ "\n" ++ render funcs (n+1)
 
 --------------------------------------------------------------------------------
